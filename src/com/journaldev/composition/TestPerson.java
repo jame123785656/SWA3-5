38c065e9e352e487c8bf66c9da815f4c1e4357fd
unknown <61955070+jame123785656@users.noreply.github.com>
2021-07-19 15:53:42 +0700

    Initial commit

---
+++ src/com/journaldev/composition/TestPerson.java
@@ -0,0 +1,8 @@
+package com.journaldev.composition;
+
+public class TestPerson {
+	 public static void main(String[] args) {
+	        Person person = new Person();
+	        long salary = person.getSalary();
+	    }
+}
